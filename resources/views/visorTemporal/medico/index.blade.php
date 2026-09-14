928c5b6550ce72bb0a49ee2bc90ffc1b590331f9
CEOSALUD DEV <dev.ceosalud@gmail.com>
2026-09-14 13:00:45 -0500

    LLAMADOR

--- resources/views/visorTemporal/medico/index.blade.php
+++ resources/views/visorTemporal/medico/index.blade.php
@@ -158,6 +158,7 @@
                 </div>
                 <div style="text-align: right;">
                     <strong>Fecha:</strong> 12/09/2026
+                    <a class="btn-sm btn-warning" href="{{ route('doctor.temporal.index') }}">Traer Datos</a>
                 </div>
             </div>
 
